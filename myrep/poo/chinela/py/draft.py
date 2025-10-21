77296dac541edf556a04f7a39657311103b93255
Iurywkv <iury.wkv@gmail.com>
2025-10-21 20:17:09 +0000

    envio chinela

--- myrep/poo/chinela/py/draft.py
+++ myrep/poo/chinela/py/draft.py
@@ -1,3 +1,18 @@
-class chinela:
-    def __init__
+class Chinela:
+    def __init__(self):
+        self.__tamanho = 0
+
+    def getTamanho(self):
+        return self.__tamanho
+    
+    def setTamanho(self, valor: int):
+        self.__tamanho = valor
+
+chinela = Chinela()
+while chinela.getTamanho() == 0:
+    print("Digite seu tamanho de chinela")
+    tamanho = int(input())
+
+    chinela.setTamanho(tamanho)
+    print("Parabens, você comprou uma chinela do tamanho", chinela.getTamanho())J
  
